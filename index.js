48532d1efb8d76fe44e74f0a6d952768c223a379
Abhiraj412 <abhangparth@gmail.com>
2024-06-09 17:58:37 +0530

    soved the cors error by sending the html file to the backend file and running the backend server instead of the front end page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
   const express = require('express');
   const bodyParser = require('body-parser');
+  const path = require("path")
   
   const app = express();
   
@@ -30,10 +31,16 @@
       res.status(200).send();
     }
   });
+
+  app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+  })
+
   
   // for all other routes, return 404
   app.use((req, res, next) => {
     res.status(404).send();
   });
 
+  
   app.listen(3000);
